658bacef8fd93a08f70f7faa04a01bf6dfd6aeda
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2020-11-18 15:07:36 +0100

    Some help from Luis

--- d2/src/components/LatesRelease.jsx
+++ d2/src/components/LatesRelease.jsx
@@ -1,19 +1,33 @@
 import React, { Component } from "react"
 
 import fantasy from "../data/fantasy.json"
+import history from "../data/history.json"
 
-import {Container, Row, Col, Card, Button} from "react-bootstrap"
+import {Container, Row, Col, Card, Button, Dropdown} from "react-bootstrap"
 
 class LatesRelease extends Component{
-  
+  state={
+    listLibri: history,
+  }
   render(){
     
-    console.log("fantasy;", fantasy)
+    console.log(this.state)
 
     return(
       <Container>
+        <Dropdown>
+  <Dropdown.Toggle variant="success" id="dropdown-basic">
+    Dropdown Button
+  </Dropdown.Toggle>
+
+  <Dropdown.Menu>
+    <Dropdown.Item onClick={()=> this.setState({listLibri: fantasy})}>Fantasy</Dropdown.Item>
+    <Dropdown.Item onClick={()=> this.setState({listLibri: history})}>History</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+
         <Row>
-          {fantasy.map(book=>
+          {this.state.listLibri.map(book=>
         <Col xs={4} className="my-5">
         <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src={book.img} />
